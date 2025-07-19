efb7caa281af3d172bc94684ac638baf9299ed54
Kunal Chheda <kunalchheda13@gmail.com>
2025-07-19 16:20:54 +0000

    When I open the side bar. Automatically the keyboard pops up and search

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -61,6 +62,7 @@ const SheetContent = React.forwardRef<
     <SheetOverlay />
     <SheetPrimitive.Content
       ref={ref}
+      onOpenAutoFocus={(e) => e.preventDefault()}
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
